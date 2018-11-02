b6e6bbc385923b8702c04da1311bd020941d595f
Parker <parker.tichko@gmail.com>
2018-11-02 12:58:29 -0400

    Purcell Parameters and generate theo FFT
    Updated purcell parameters and fixed the equation to linearly ramp down generators to a drop-out frequency

--- FFRFine_parameters_Purcell.m
+++ FFRFine_parameters_Purcell.m
@@ -3,22 +3,23 @@
 
 %Stimulus/ response frequencies
 %frequency = [16 17 18 19 21 22 23 25 26 28 29 31 33 35 37 39 41 44 46 49 52 55 58 62 65 69 73 78 82 87 93 98 104 110 117 123 131 139 147 156 165 175 185 196 208 220 233 247 262 277 294 311 330 349 370 392 415 440 466 494 523 554 587 622 659 698 740 831 880];
-frequency = [16 17 18 19 21 22 23 25 26 28 29 31 33 35 37 39 41 44 46 49 52 55 58 62 65 69 73 78 82 87 93 98 104 110];
+frequency = 20:1:100;
 
 %Latencies: Purcell Model
-latency = [7.3 0 0 0 29 0];
+latency = [0 7.3 0 0 29 0];
 
 %Scale amplitude
 stimamp = frequency./frequency;
 
 %Amplitude values
-amp = [0.35 0 0 0 0.85 0];
+% G > 4 will have amplitude ramped down linearly
+amp = [0 0.35 0 0 0.85 0];
 
 %Drop out frequencies
-dropoutfreq = [880 0 0 0 95 0];  %drop out frequenices for each generator. 
+dropoutfreq = [0 110 0 0 110 0];  %drop out frequenices for each generator. 
 
 %LPF cut-off frequency: Filter applied to the aggregate waveform prior to FFT.
-LP = 200; 
+LP = 100; 
 
 %Change latencies by maximum pertange in and in % intervals
 maxperc  = 0; %total perc change
@@ -29,10 +30,10 @@ gen = [];
 
 %Initalize figure parameters and counter
 if maxperc == 0 
-    colorVec = jet(1);
+    colorVec = jet(1); %color vector for plotting
     counter = 1;
 else
-colorVec = jet(maxperc/percinterv); %color vector for plotting
+colorVec = jet(maxperc/percinterv); 
 counter = maxperc/percinterv;
 end
 
--- generate_TheoFFRTalk.m
+++ generate_TheoFFRTalk.m
@@ -85,7 +85,13 @@ for f = 1:length(frequency)
         if Fn<dropoutfreq(L)  % ramps down the frequency 
             if L>4
             if Fn>=dropoutfreq(L)/2
-                matrix(:,L) = matrix(:,L) * (1-(Fn./dropoutfreq(L)));
+                %matrix(:,L) = matrix(:,L) * (1-(Fn./dropoutfreq(L)));
+                %%this will immediately halve the amplitude instead of
+                %%linear ramp
+                
+                matrix(:,L) = matrix(:,L) * (2 - (2 * (Fn./dropoutfreq(L))));
+                %This should now linearly ramp it
+                
             end
             end
          
